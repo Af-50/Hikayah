ca62651369ac06ff5458b539b6cd89946abdec2c
fatemah <fatemah.bu50@gmail.com>
2025-05-06 00:44:57 +0300

    done

--- basic3.js
+++ basic3.js
@@ -9,14 +9,14 @@ window.onload = () => {
             // Add a model to the north of the initial GPS position
             const comp = document.createElement("a-entity"); //modell
             comp.setAttribute('gps-new-entity-place', {
-                latitude: 49.565552165777945,
-                longitude:27.155829202409045
+                latitude: e.detail.position.latitude + 0.001,
+                longitude: e.detail.position.longitude
             });
             
             const model = document.createElement("a-entity");
             model.setAttribute('gltf-model', './assets/white_man/scene.gltf')
             model.object3D.scale.set(20,20,20);            
-            model.object3D.rotation.set(90);
+            //model.object3D.rotation.set(90);
             model.setAttribute("position", {
                 x: 0,
                 y: 20,
