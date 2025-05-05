ea7e46c383986b450e4ef0db64d6f0e45cad50f3
fatemah <fatemah.bu50@gmail.com>
2025-05-05 22:54:23 +0300

    done

--- basic3.js
+++ basic3.js
@@ -6,32 +6,19 @@ window.onload = () => {
     el.addEventListener("gps-camera-update-position", e => {
         if(!testEntityAdded) {
             alert(`Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`);
-            // Add a box to the north of the initial GPS position
+            // Add a model to the north of the initial GPS position
             const model = document.createElement("a-entity"); //modell
-            const box = document.createElement("a-box");  // box
             
             
             model.setAttribute('gltf-model', './assets/white_man/scene.gltf')
             model.object3D.scale.set(20,20,20);
             //entity.setAttribute('material', { color: 'red' } );
             model.setAttribute('gps-new-entity-place', {
-                latitude: e.detail.position.latitude + 0.001,
-                longitude: e.detail.position.longitude
+                latitude: 49.565552165777945,
+                longitude:27.155829202409045
             });
-            model.object3D.rotation.set(90);
-            box.setAttribute("scale", {
-                x: 20, 
-                y: 20,
-                z: 20
-            });
-            box.setAttribute('material', { color: 'red' } );
-            box.setAttribute('gps-new-entity-place', {
-                
-                latitude: e.detail.position.latitude + 0.001,
-                longitude: e.detail.position.longitude +0.001
-            });
+            
             document.querySelector("a-scene").appendChild(model);
-            document.querySelector("a-scene").appendChild(box);
         }
         testEntityAdded = true;
     });
