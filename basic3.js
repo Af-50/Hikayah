43f9eeba16754966b396b24c1cf03cecc99bf3c1
fatemah <fatemah.bu50@gmail.com>
2025-05-06 01:41:00 +0300

    done

--- basic3.js
+++ basic3.js
@@ -8,7 +8,7 @@ window.onload = () => {
             alert(`Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`);
             // Add a model to the north of the initial GPS position
             const comp = document.createElement("a-entity"); 
-            /*
+            
             comp.setAttribute('gps-new-entity-place', {
                 latitude: e.detail.position.latitude + 0.001,
                 longitude: e.detail.position.longitude
@@ -17,37 +17,80 @@ window.onload = () => {
             const model = document.createElement("a-entity");
             model.setAttribute('gltf-model', './assets/white_man/scene.gltf')
             model.object3D.scale.set(20,20,20); 
-            model.setAttribute('gps-new-entity-place',{ //this
+            
+            /*model.setAttribute('gps-new-entity-place',{ //this
                 latitude: e.detail.position.latitude + 0.001,
                 longitude: e.detail.position.longitude
-            });           */
+            });           
             //model.object3D.rotation.set(90);
-           /*
+           /**/
             model.setAttribute("position", {
                 x: 0,
                 y: 0,
                 z: 0
             });
-*/
+
+           
             const text = document.createElement("a-entity");
             const textScale = 100;
             text.setAttribute("look-at", "[gps-new-camera]");
             
-            text.setAttribute("text", "value: this is home");
-            //text.setAttribute("align", "center");
+            text.setAttribute("value", " this is home");
+            text.setAttribute("align", "center");
 
-            text.setAttribute('gps-new-entity-place',{
-                latitude: e.detail.position.latitude + 0.001,
-                longitude: e.detail.position.longitude + 0.0005
-            });
+            
+            comp.appendChild(model);
+            comp.appendChild(text);
 
-            //comp.appendChild(model);
-            //comp.appendChild(text);
-
-            //document.querySelector("a-scene").appendChild(comp);
+            document.querySelector("a-scene").appendChild(comp);
             //document.querySelector("a-scene").appendChild(model);//this
-            document.querySelector("a-scene").appendChild(text);//this
+            //document.querySelector("a-scene").appendChild(text);//this
+        }
+        testEntityAdded = true; 
+        model.addEventListener("model-error",function mm(){
+            alert("did not loead");
+        } );
+
+    });
+};
+
+/*
+window.onload = () => {
+    let testEntityAdded = false;
+
+    const el = document.querySelector("[gps-new-camera]");
+
+    el.addEventListener("gps-camera-update-position", e => {
+        if(!testEntityAdded) {
+            alert(`Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`);
+            // Add a box to the north of the initial GPS position
+            const model = document.createElement("a-entity"); //modell
+            const box = document.createElement("a-box");  // box
+            
+            
+            model.setAttribute('gltf-model', './assets/white_man/scene.gltf')
+            model.object3D.scale.set(20,20,20);
+            //entity.setAttribute('material', { color: 'red' } );
+            model.setAttribute('gps-new-entity-place', {
+                latitude: e.detail.position.latitude + 0.001,
+                longitude: e.detail.position.longitude
+            });
+            //model.object3D.rotation.set(90);
+            box.setAttribute("scale", {
+                x: 20, 
+                y: 20,
+                z: 20
+            });
+            box.setAttribute('material', { color: 'red' } );
+            box.setAttribute('gps-new-entity-place', {
+                
+                latitude: e.detail.position.latitude + 0.001,
+                longitude: e.detail.position.longitude +0.001
+            });
+            document.querySelector("a-scene").appendChild(model);
+            document.querySelector("a-scene").appendChild(box);
         }
         testEntityAdded = true;
     });
 };
+*/
